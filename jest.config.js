e3f771f0aeca6378e2f398f29208911fe78de3a5
rejo132 <enoch.luke1@student.moringaschool.com>
2025-07-16 11:15:12 +0300

    fixed jest issues

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
+// jest.config.js
 module.exports = {
     testEnvironment: 'jsdom',
     transform: {
-      '^.+\\.[jt]sx?$': 'babel-jest',
+      '^.+\\.jsx?$': 'babel-jest'
     },
     transformIgnorePatterns: [
-      '/node_modules/(?!axios)/',
+      '/node_modules/(?!axios)' // allow transforming axios ESM
     ],
     moduleFileExtensions: ['js', 'jsx'],
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
   };
   
